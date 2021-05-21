5a7a3646194851ddd4edf6b27ad99eecbd388ded
J Alfonso Martínez Baeza <mtz.alfonso96@gmail.com>
2021-05-21 01:57:13 -0500

    Configurando Express

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+const express = require('express');
+
+const PORT = process.env.PORT || 4000;
+
+// Se crea el servidor
+const app = express();
+
+// Rutas
+
+app.get('/', (req, res) => {
+  res.json({
+    ok: true,
+  });
+});
+
+// Escuchando peticiones
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
