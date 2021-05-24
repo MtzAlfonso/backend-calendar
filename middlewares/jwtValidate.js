706ac7a985b783dbdc3d36a40bac1b991f00ef60
J Alfonso Martínez Baeza <mtz.alfonso96@gmail.com>
2021-05-24 05:26:25 -0500

    Modificando la respueste del middleware jwtValidate

--- middlewares/jwtValidate.js
+++ middlewares/jwtValidate.js
@@ -5,7 +5,7 @@ const jwtValidate = (req = request, res = response, next) => {
   const token = req.header('x-token');
 
   if (!token) {
-    return res.status(401).json({
+    return res.json({
       ok: false,
       msg: 'No hay usuario autenticado',
     });
